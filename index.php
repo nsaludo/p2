7cbd5b3038cdc46ac4fd3e6bc07f89fd852c2dd4
Nora Saludo <nor.saludo@oracle.com>
2016-02-16 20:55:02 -0500

    Initial commit for xkcd password generator

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nora Saludo P2 DWA xkcd password generator</title>
+
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/mystyles.css">
+
+    <!--Include php script to process _POST-->
+    <?php require 'logic.php'; ?>
+
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" id="home-navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="" class="navbar-brand">dwa15</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="http://p1.donengs.com">Home</a>
+                    <li><a href="http://p3.donengs.com">Lorem Ipsum Generator</a>
+                    <li><a href="http://p3.donengs.com">Random User Generator</a>
+                    <li><a href="http://p4.donengs.com">Final Project</a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--End Navbar-->
+
+    <footer>Copyright &copy; 2016 Nora Saludo</footer>
+</body>
+</html>
+
